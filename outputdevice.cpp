c2cfc5d914175ffc3dc76a2a9eb41fd3776dbdb2
yonatan <or.yonatan@gmail.com>
2013-05-06 19:29:10 +0300

    aghahhahhahahha

--- outputdevice.cpp
+++ outputdevice.cpp
@@ -38,8 +38,16 @@
 
 using namespace std;
 
+
+pthread_mutex_t printCounterMut = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t closeMutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
 //The mutex that makes sure only one initialization process goes at the same time
-pthread_mutex_t initMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t initMutex;
+
+
 //The daemon thread that does all the printing
 pthread_t daemonThread;
 
@@ -51,64 +59,64 @@ unique_ptr<TaskList> allTasks;
 ofstream diskFile;
 long printCounter = 0;
 
-int lockAndInit(pthread_mutex_t * mut)
-{
-	int lockStatus =  pthread_mutex_lock(mut) ;
-	if (lockStatus == EINVAL)
-	{
-		pthread_mutex_init(mut,NULL);
-		return pthread_mutex_lock(mut);
-	}
-	return lockStatus;
-}
 
 //Helper function that destroys all the resources and frees the memory
 void closeEverything()
 {
+	pthread_mutex_lock(&initMutex);
 	if (diskFile != NULL)
 	{
 		diskFile.close();
 	}
+
 	initialized = false;
-	pthread_mutex_destroy(&initMutex);
+	pthread_mutex_unlock(&initMutex);
 }
 
 //The entry point to the writing daemon thread
 void* writingFunc(void *)
 {
 	shared_ptr<Task> firstTask;
-
+	pthread_mutex_lock(&initMutex);
 	while (initialized)
 	{
-
-		if (allTasks->idsLeft())
+		pthread_mutex_unlock(&initMutex);
+		if ((firstTask  = allTasks->popTask()) != NULL)
 		{
 
-			firstTask = allTasks->front();
 			diskFile.write(&(firstTask->data)[0],firstTask->length);
 			if(diskFile.fail())
 			{
 				cerr << SYS_ERROR_MESSAGE << endl;
 				exit (-2);
 			}
+			pthread_mutex_lock(&printCounterMut);
 			printCounter++;
 			allTasks->done(firstTask->id);
-			allTasks->popTask();
 			pthread_cond_broadcast(&(firstTask->sig));
-
+			pthread_mutex_unlock(&printCounterMut);
 		}
-		else if (closing)
+		else
 		{
-			closeEverything();
-			return NULL;
+			pthread_mutex_lock(&closeMutex);
+			if (closing)
+			{
+				closeEverything();
+				closing = false;
+				pthread_mutex_unlock(&closeMutex);
+				return NULL;
+			}
+			pthread_mutex_unlock(&closeMutex);
 		}
+
+		pthread_mutex_lock(&initMutex);
 	}
 	return NULL;
 }
 
 int initdevice(char *filename)
 {
-	if (lockAndInit(&initMutex))
+	if (pthread_mutex_lock(&initMutex))
 	{
 		cerr << SYS_ERROR_MESSAGE << endl;
 		return FAIL;
@@ -136,12 +144,12 @@ int initdevice(char *filename)
 		return FAIL;
 
 	}
+	initialized = true;
 	if (pthread_mutex_unlock(&initMutex))
 	{
 		cerr << SYS_ERROR_MESSAGE << endl;
 		return FAIL;
 	}
-	initialized = true;
 	return SUCCESS;
 }
 
@@ -149,11 +157,17 @@ int write2device(char *buffer, int length)
 {
 	int newId;
 	//initdevice must be called before calling any function
+	pthread_mutex_lock(&closeMutex);
+	pthread_mutex_lock(&initMutex);
 	if (!initialized || closing)
 	{
+		pthread_mutex_unlock(&closeMutex);
+		pthread_mutex_unlock(&initMutex);
 		cerr << LIB_ERROR_MESSAGE << endl;
 		return FAIL;
 	}
+	pthread_mutex_unlock(&closeMutex);
+	pthread_mutex_unlock(&initMutex);
 	vector<char> data(buffer,buffer+length);
 	newId = allTasks->addTask(data,length);
 	return newId;
@@ -166,26 +180,15 @@ int flush2device(int task_id)
 		cerr << LIB_ERROR_MESSAGE << endl;
 		return TID_NOT_FOUND_ERROR;
 	}
+	pthread_mutex_lock(&initMutex);
 	if (!initialized)
 	{
+		pthread_mutex_unlock(&initMutex);
 		cerr << LIB_ERROR_MESSAGE << endl;
 		return FAIL;
 	}
-	location loc = allTasks->findTid(task_id);
-	switch (loc)
-	{
-	case NOT_FOUND:
-		cerr << LIB_ERROR_MESSAGE << endl;
-		return TID_NOT_FOUND_ERROR;
-	case HISTORY:
-		return FLUSH_SUCCESFUL;
-	case RUNNING:
-		lockAndInit(allTasks->getSignalMutex(task_id));
-		pthread_cond_wait(allTasks->getSignal(task_id),
-				allTasks->getSignalMutex(task_id));
-		return FLUSH_SUCCESFUL;
-	}
-	return FAIL; // unreachable , but eclipse is bugging me
+	pthread_mutex_unlock(&initMutex);
+	return allTasks->waitToEnd(task_id);
 
 }
 
@@ -206,35 +209,53 @@ int wasItWritten(int task_id)
 
 int howManyWritten()
 {
+	pthread_mutex_lock(&initMutex);
 	if (!initialized)
 	{
+		pthread_mutex_unlock(&initMutex);
 		return FAIL;
 	}
-	if (printCounter > INT_MAX) return INT_MIN;
-	return printCounter;
+	pthread_mutex_unlock(&initMutex);
+	pthread_mutex_lock(&printCounterMut);
+	int pc = printCounter;
+	if (printCounter > INT_MAX)
+	{
+		pthread_mutex_unlock(&printCounterMut);
+		return INT_MIN;
+	}
+	pthread_mutex_unlock(&printCounterMut);
+	return pc;
 }
 
 void closedevice()
 {
-	//TODO - is there a possibility of error we're missing? The one that should cause exit....
+	pthread_mutex_lock(&closeMutex);
 	closing = true;
+	pthread_mutex_unlock(&closeMutex);
 }
 
 int wait4close()
 {
 	void* ret = NULL;
-
+	pthread_mutex_lock(&closeMutex);
+	pthread_mutex_lock(&initMutex);
 	if (!(closing && initialized))
 	{
+		pthread_mutex_unlock(&closeMutex);
+		pthread_mutex_unlock(&initMutex);
 		cerr << LIB_ERROR_MESSAGE << endl;
 		return WAIT_FOR_CLOSE_TO_EARLY;
 	}
+	pthread_mutex_unlock(&initMutex);
+	pthread_mutex_unlock(&closeMutex);
 	if(pthread_join(daemonThread,&ret))
 	{
 		cerr << SYS_ERROR_MESSAGE << endl;
 		return FAIL;
 	}
+	pthread_mutex_lock(&closeMutex);
 	closing = false;
+	pthread_mutex_unlock(&closeMutex);
 	return WAITFORCLOSE_SUCCESSFUL;
 }
 
